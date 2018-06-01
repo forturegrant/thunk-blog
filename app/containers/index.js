04d57d55ad5fc543142ed959165660f6b016cea3
1209095474@qq.com <a158192305910>
2018-06-01 20:33:47 +0800

    登录注册

--- app/containers/index.js
+++ app/containers/index.js
@@ -1,49 +1,72 @@
-import React,{Component,PropTypes} from 'react'
+import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux'
 import {
-	BrowserRouter as Router,
-	Route,
-	Switch,
-	Redirect
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect
 } from 'react-router-dom'
 import '../style/style.less'
 import Login from './login'
+import Logined from './logined'
 import Register from './register'
-import {Carousel,Tabs} from 'antd'
+import {Carousel, Tabs} from 'antd'
+
 const TabPane = Tabs.TabPane;
 
-export default class App extends Component{
-	constructor(props){
-		super(props)
-	}
-	componentWillMount(){
-		/*const token = localStorage.getItem('token');
-		if(!token){
+class App extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    componentWillMount() {
+        /*const token = localStorage.getItem('token');
+        if(!token){
             history.pushState({},0,'http://'+window.location.host+'/#/login');
         }*/
-	}
-	render(){
-		return(
-			<div>
-				<Carousel autoplay>
-					<div>
-						<img src="static/banner_1.png" />
-					</div>
-					<div>
-						<img src="static/banner_2.png" />
-					</div>
-					<div>
-						<img src="static/banner_3.png" />
-					</div>
-				</Carousel>
-				<Tabs>
-					<TabPane tab="登录" key="1">
-						<Login />
-					</TabPane>
-					<TabPane tab="注册" key="2">
-						<Register />
-					</TabPane>
-				</Tabs>
-			</div>
-			)
-	}
+    }
+
+    render() {
+        const {userInfo} = this.props;
+        return (
+            <div>
+                <Carousel autoplay>
+                    <div>
+                        <img src="static/banner_1.png"/>
+                    </div>
+                    <div>
+                        <img src="static/banner_2.png"/>
+                    </div>
+                    <div>
+                        <img src="static/banner_3.png"/>
+                    </div>
+                </Carousel>
+                {userInfo ? <Logined /> :
+                    <Tabs defaultActiveKey="1" tabBarStyle={{textAlign: 'center'}} className='container'>
+                        <TabPane tab="登录" key="1">
+                            <Login/>
+                        </TabPane>
+                        <TabPane tab="注册" key="2">
+                            <Register/>
+                        </TabPane>
+                    </Tabs>
+                }
+            </div>
+        )
+    }
 }
+
+
+const mapStateToProps = (state) => ({
+    userInfo: state.userInfo
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    dispatch: 1
+})
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App)
+
---
+++ app/containers/logined/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Button} from 'antd'
+
+const Logined = (props) => (
+    <div className='container'>
+        {/*<img src={require('./timg.jpeg')}/>*/}
+        <p>欢迎：{props.userInfo.username}</p>
+        <p className='centerP'>光临我的博客~</p>
+        {props.userInfo.userType === 'admin' ?
+            <Button onClick={() => props.history.push('/admin')} type="primary">点击进入管理页面</Button> : null}
+        <Button onClick={() => props.logout()} type="primary">退出登录</Button>
+    </div>
+);
+
+export default Logined;
